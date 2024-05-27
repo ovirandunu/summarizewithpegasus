1fd859c1013f9bc5267d036808f9785440dac545
ovi <ovirandunu@gmail.com>
2024-05-27 07:48:45 +0200

    chore: Update trainer.sh script with GPU configuration and dependencies

--- model-trainer/trainer.sh
+++ model-trainer/trainer.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=10:00:00
+#SBATCH --output=pegasus-trainer.out
+#SBATCH -J spred2 
 
+cd $HOME/tm/tmgp/model-trainer
 
-
-
-
+source $HOME/tm/tmgp/venv/bin/activate
 
 pip install transformers[sentencepiece] datasets sacrebleu rouge_score py7zr -q
 pip uninstall transformers accelerate -y
 pip install transformers[torch] accelerate -U
+
+python trainer.py
