79e2f89e7e140298f751fff7d56e9074604849d9
ovi <ovirandunu@gmail.com>
2024-05-31 13:15:26 +0200

    chore: Refactor trainer.py and check.ipynb files

---
+++ model-trainer/utils.py
@@ -0,0 +1,29 @@
+# Helper function to split data into batch-sized chunks
+def generate_batch_sized_chunks(list_of_elements, batch_size):
+    """Yield successive batch-sized chunks from list_of_elements."""
+    for i in range(0, len(list_of_elements), batch_size):
+        yield list_of_elements[i: i + batch_size]
+
+# Function to evaluate the summarization model on the test data
+def calculate_metric_on_test_ds(dataset, metric, model, tokenizer, batch_size=8, device=device, column_text="utterance", column_summary="summary"):
+    model.eval()
+    article_batches = list(generate_batch_sized_chunks(dataset[column_text], batch_size))
+    target_batches = list(generate_batch_sized_chunks(dataset[column_summary], batch_size))
+
+    for article_batch, target_batch in tqdm(zip(article_batches, target_batches), total=len(article_batches)):
+        inputs = tokenizer(article_batch, max_length=1024, truncation=True, padding="max_length", return_tensors="pt").to(device)
+
+        with torch.no_grad():
+            summaries = model.generate(input_ids=inputs["input_ids"].to(device),
+                                       attention_mask=inputs["attention_mask"].to(device),
+                                       length_penalty=0.8, num_beams=8, max_length=128)
+
+        decoded_summaries = [tokenizer.decode(s, skip_special_tokens=True, clean_up_tokenization_spaces=True) for s in summaries]
+        decoded_summaries = [d.replace("", " ") for d in decoded_summaries]
+
+        metric.add_batch(predictions=decoded_summaries, references=target_batch)
+        del inputs
+        torch.cuda.empty_cache() if device == 'cuda' else None
+
+    score = metric.compute()
+    return score
