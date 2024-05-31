c3b50669260740171668ad7f5d60ed20919c0e9e
ovi <ovirandunu@gmail.com>
2024-05-31 10:55:48 +0200

    add sh

---
+++ model-trainer/empathetic.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=30:00:00
+#SBATCH --output=empathetic-trainer.out
+#SBATCH -J spred2 
+
+cd $HOME/tm/empathetic/tmgp/model-trainer
+
+source $HOME/tm/venv/bin/activate
+
+pip install transformers[sentencepiece] datasets sacrebleu rouge_score py7zr -q
+pip uninstall transformers accelerate -y
+pip install transformers[torch] accelerate -U
+
+python empathetic_summaries.py
